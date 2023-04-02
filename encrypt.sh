6949ab8f1677911fbe98221a7f2c7498ec1bb5a5
MTRNord <mtrnord1@gmail.com>
2023-04-02 16:31:23 +0200

    move to externalDatabase with media-repo

--- encrypt.sh
+++ encrypt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sops --age=age1esjyg2qfy49awv0ptkzvpk425adczjr38m37w2mmcahzc4p8n54sll2nzh --encrypt --encrypted-regex '^(data|stringData)$' --in-place "$1"
+sops --age=age1esjyg2qfy49awv0ptkzvpk425adczjr38m37w2mmcahzc4p8n54sll2nzh --encrypt --encrypted-regex '^(data|stringData|password)$' --in-place "$1"
