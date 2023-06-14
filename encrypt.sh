2d9faf4a6fc6bd7de88aed94a90df8e9b0b0f605
MTRNord <mtrnord1@gmail.com>
2023-06-14 17:51:30 +0200

    create a openproject setup

--- encrypt.sh
+++ encrypt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sops --age=age1esjyg2qfy49awv0ptkzvpk425adczjr38m37w2mmcahzc4p8n54sll2nzh --encrypt --encrypted-regex '^(data|stringData|password|registration_shared_secret|shared_secret)$' --in-place "$1"
+sops --age=age1esjyg2qfy49awv0ptkzvpk425adczjr38m37w2mmcahzc4p8n54sll2nzh --encrypt --encrypted-regex '^(data|stringData|password|postgresPassword|registration_shared_secret|shared_secret|secret)$' --in-place "$1"
