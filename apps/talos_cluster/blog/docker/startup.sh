2769b444e794e8e3046fa2b8b612523baafeb2f1
MTRNord <MTRNord@users.noreply.github.com>
2025-10-14 20:42:54 +0200

    try setting up a blog
    Signed-off-by: MTRNord <MTRNord@users.noreply.github.com>

---
+++ apps/talos_cluster/blog/docker/startup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+WP_CONFIG="/var/www/html/wordpress/wp-config.php"
+
+sed -i "s/\${DB_NAME}/${DB_NAME}/g" $WP_CONFIG
+sed -i "s/\${DB_USER}/${DB_USER}/g" $WP_CONFIG
+sed -i "s/\${DB_PASSWORD}/${DB_PASSWORD}/g" $WP_CONFIG
+sed -i "s/\${DB_HOST}/${DB_HOST}/g" $WP_CONFIG
+sed -i "s/\${AUTH_KEY}/${AUTH_KEY}/g" $WP_CONFIG
+sed -i "s/\${SECURE_AUTH_KEY}/${SECURE_AUTH_KEY}/g" $WP_CONFIG
+sed -i "s/\${LOGGED_IN_KEY}/${LOGGED_IN_KEY}/g" $WP_CONFIG
+sed -i "s/\${NONCE_KEY}/${NONCE_KEY}/g" $WP_CONFIG
+sed -i "s/\${AUTH_SALT}/${AUTH_SALT}/g" $WP_CONFIG
+sed -i "s/\${SECURE_AUTH_SALT}/${SECURE_AUTH_SALT}/g" $WP_CONFIG
+sed -i "s/\${LOGGED_IN_SALT}/${LOGGED_IN_SALT}/g" $WP_CONFIG
+sed -i "s/\${NONCE_SALT}/${NONCE_SALT}/g" $WP_CONFIG
+
+service php8.4-fpm start
+nginx -g 'daemon off;'
---
+++ apps/talos_cluster/blog/docker/wp-config.php
@@ -0,0 +1,69 @@
+<?php
+define( 'DB_NAME', '${DB_NAME}' );
+define( 'DB_USER', '${DB_USER}' );
+define( 'DB_PASSWORD', '${DB_PASSWORD}' );
+define( 'DB_HOST', '${DB_HOST}' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '${AUTH_KEY}' );
+define( 'SECURE_AUTH_KEY',  '${SECURE_AUTH_KEY}' );
+define( 'LOGGED_IN_KEY',    '${LOGGED_IN_KEY}' );
+define( 'NONCE_KEY',        '${NONCE_KEY}' );
+define( 'AUTH_SALT',        '${AUTH_SALT}' );
+define( 'SECURE_AUTH_SALT', '${SECURE_AUTH_SALT}' );
+define( 'LOGGED_IN_SALT',   '${LOGGED_IN_SALT}' );
+define( 'NONCE_SALT',       '${NONCE_SALT}' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ *
+ * At the installation time, database tables are created with the specified prefix.
+ * Changing this value after WordPress is installed will make your site think
+ * it has not been installed.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+    
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', __DIR__ . '/' );
+}
+    
+require_once ABSPATH . 'wp-settings.php';
